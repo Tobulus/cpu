1e96f96aff766613222e927b25734879c5afc345
tobsucht <tobiasbagg@googlemail.com>
2020-12-12 13:37:32 +0100

    update naming

--- ctrl_unit/ctrl_unit.v
+++ ctrl_unit/ctrl_unit.v
@@ -1,30 +1,30 @@
 `include "../alu/ops.vh"
 
-module ctrl_unit(clk, 
-    reset,
+module ctrl_unit(I_clk, 
+    I_reset,
     I_instruction,
     I_mem_ready,
     I_data_ready,
-    state,
+    O_state,
 O_execute);
 
 /* verilator lint_off UNUSED */
-input clk, I_instruction, I_mem_ready, I_data_ready, reset;
-output state, O_execute;
+input I_clk, I_instruction, I_mem_ready, I_data_ready, I_reset;
+output O_state, O_execute;
 
 reg[15:0] I_instruction;
-reg[5:0] state;
+reg[5:0] O_state;
 reg O_execute;
 
 reg mem_wait = 0;
 
-always @(posedge clk)
+always @(posedge I_clk)
 begin: CTRL_UNIT
-    if (reset == 1) 
+    if (I_reset == 1) 
     begin
-        state <= 1;
+        O_state <= 1;
     end
-    else if (state == 6'b000001)
+    else if (O_state == 6'b000001)
     begin
         // fetch
         if (I_mem_ready == 1 && mem_wait == 0)
@@ -38,21 +38,21 @@ begin: CTRL_UNIT
             if (I_data_ready == 1)
             begin
                 mem_wait <= 0;
-                state <= 6'b000010;
+                O_state <= 6'b000010;
             end
         end
     end
-    else if (state == 6'b000010)
+    else if (O_state == 6'b000010)
     begin
         // decode
-        state <= 6'b000100;
+        O_state <= 6'b000100;
     end
-    else if (state == 6'b000100)
+    else if (O_state == 6'b000100)
     begin
         // register read
-        state <= 6'b001000;
+        O_state <= 6'b001000;
     end
-    else if (state == 6'b001000)
+    else if (O_state == 6'b001000)
     begin
         // execute
         if (I_instruction[15:12] == WRITE || I_instruction[15:12] == READ)
@@ -62,14 +62,14 @@ begin: CTRL_UNIT
                 O_execute <= 1;
                 mem_wait <= 1;
             end
-            state <= 6'b010000; 
+            O_state <= 6'b010000; 
         end
         else 
         begin
-            state <= 6'b100000;
+            O_state <= 6'b100000;
         end
     end
-    else if (state == 6'b010000)
+    else if (O_state == 6'b010000)
     begin
         // store
         if (I_mem_ready == 1 && mem_wait == 0)
@@ -83,14 +83,14 @@ begin: CTRL_UNIT
             if (I_instruction[15:12] == WRITE || I_data_ready == 1)
             begin
                 mem_wait <= 0;
-                state <= 6'b100000;
+                O_state <= 6'b100000;
             end
         end
     end
-    else if (state == 6'b100000)
+    else if (O_state == 6'b100000)
     begin
         // register write
-        state <= 6'b000001;
+        O_state <= 6'b000001;
     end
 end
 
