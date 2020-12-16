e985798b3c5a563e92c73e66ca86fc175b6dc43e
tobsucht <tobiasbagg@googlemail.com>
2020-12-16 10:53:30 +0100

    add assembler

---
+++ assembler/assembler.py
@@ -0,0 +1,151 @@
+import re, sys
+
+LABEL   = "(\w+):"
+ADD     = "add r(\d), r(\d), r(\d)"
+SUB     = "sub r(\d), r(\d), r(\d)"
+NOT     = "not r(\d), r(\d)"
+OR      = "or r(\d), r(\d)"
+AND     = "and r(\d), r(\d)"
+XOR     = "xor r(\d), r(\d)"
+READ    = "read r(\d), r(\d)"
+WRITE   = "write r(\d), r(\d)" 
+LOAD    = "not r(\d), (\d{1,3})"
+CMP     = "cmp r(\d), r(\d), r(\d)"
+SHIFTL  = "shftl r(\d), r(\d)"
+SHIFTR  = "shftr r(\d), r(\d)"
+JMP     = "jmp r(\d), -?(\d{1,3})"
+JMPEQ   = "jmpeq r(\d), r(\d)"
+JMPRAGT = "jmpragt r(\d), r(\d)"
+JMPRBGT = "jmprbgt r(\d), r(\d)"
+JMPRAZ  = "jmpraz r(\d), r(\d)"
+JMPRBZ  = "jmprbz r(\d), r(\d)"
+
+SHIFT_OPCODE = 2
+SHIFT_RD     = 9
+SHIFT_RA     = 5
+SHIFT_RB     = 2
+
+
+with open(sys.argv[1]) as f:
+    pos = 0
+    labels = {}
+    output = []
+    lines = [line[:-1] for line in f.readlines()]
+
+    for line in lines:
+        instruction = 0
+
+        if match := re.fullmatch(LABEL, line):
+            if match.group(1) in labels:
+                raise Exception('Label "{}" already exists'.format(match.group(1)))
+            labels[match.group(1)] = pos
+
+        elif match := re.fullmatch(ADD, line):
+            instruction = int(match.group(1)) << SHIFT_RD \
+                          | int(match.group(2)) << SHIFT_RA \
+                          | int(match.group(3)) << SHIFT_RB
+            output.append(instruction)
+
+        elif match := re.fullmatch(SUB, line):
+            instruction = 1 << SHIFT_OPCODE \
+                    | int(match.group(1)) << SHIFT_RD \
+                    | int(match.group(2)) << SHIFT_RA \
+                    | int(match.group(3)) << SHIFT_RB
+            output.append(instruction)
+
+        elif match := re.fullmatch(OR, line):
+            instruction = 2 << SHIFT_OPCODE \
+                    | int(match.group(1)) << SHIFT_RD \
+                    | int(match.group(2)) << SHIFT_RA
+            output.append(instruction)
+
+        elif match := re.fullmatch(AND, line):
+            instruction = 3 << SHIFT_OPCODE \
+                    | int(match.group(1)) << SHIFT_RD \
+                    | int(match.group(2)) << SHIFT_RA
+            output.append(instruction)
+
+        elif match := re.fullmatch(XOR, line):
+            instruction = 4 << SHIFT_OPCODE \
+                    | int(match.group(1)) << SHIFT_RD \
+                    | int(match.group(2)) << SHIFT_RA
+            output.append(instruction)
+
+        elif match := re.fullmatch(NOT, line):
+            instruction = 5 << SHIFT_OPCODE \
+                    | int(match.group(1)) << SHIFT_RD \
+                    | int(match.group(2)) << SHIFT_RA
+            output.append(instruction)
+
+        elif match := re.fullmatch(READ, line):
+            instruction = 6 << SHIFT_OPCODE \
+                    | int(match.group(1)) << SHIFT_RA \
+                    | int(match.group(2)) << SHIFT_RB
+            output.append(instruction)
+
+        elif match := re.fullmatch(WRITE, line):
+            instruction = 7 << SHIFT_OPCODE \
+                    | int(match.group(1)) << SHIFT_RA \
+                    | int(match.group(2)) << SHIFT_RB
+            output.append(instruction)
+
+        elif match := re.fullmatch(LOAD, line):
+            instruction = 8 << SHIFT_OPCODE \
+                    | int(match.group(1)) << SHIFT_RD \
+                    | int(match.group(2)) 
+            output.append(instruction)
+
+        elif match := re.fullmatch(CMP, line):
+           instruction = 9 << SHIFT_OPCODE \
+                   | int(match.group(1)) << SHIFT_RD \
+                   | int(match.group(2)) << SHIFT_RA \
+                   | int(match.group(3)) << SHIFT_RB
+           output.append(instruction)
+
+        elif match := re.fullmatch(SHIFTL, line):
+           instruction = 10 << SHIFT_OPCODE \
+                   | int(match.group(1)) << SHIFT_RD \
+                   | int(match.group(2)) << SHIFT_RA
+           output.append(instruction)
+
+        elif match := re.fullmatch(SHIFTR, line):
+           instruction = 11 << SHIFT_OPCODE \
+                   | int(match.group(1)) << SHIFT_RD \
+                   | int(match.group(2)) << SHIFT_RA
+           output.append(instruction)
+
+        elif match := re.fullmatch(JMP, line):
+           instruction = 12 << SHIFT_OPCODE \
+                   | int(match.group(1))
+           output.append(instruction)
+
+        elif match := re.fullmatch(JMPEQ, line):
+           instruction = 13 << SHIFT_OPCODE \
+                   | int(match.group(1))
+           output.append(instruction)
+
+        elif match := re.fullmatch(JMPRAGT, line):
+           instruction = 12 << SHIFT_OPCODE \
+                   | int(match.group(1))
+           output.append(instruction)
+
+        elif match := re.fullmatch(JMPRBGT, line):
+           instruction = 12 << SHIFT_OPCODE \
+                   | int(match.group(1))
+           output.append(instruction)
+
+        elif match := re.fullmatch(JMPRAZ, line):
+           instruction = 12 << SHIFT_OPCODE \
+                   | int(match.group(1))
+           output.append(instruction)
+
+        elif match := re.fullmatch(JMPRBZ, line):
+           instruction = 12 << SHIFT_OPCODE \
+                   | int(match.group(1))
+           output.append(instruction)
+
+
+        pos += 2
+
+for op in output:
+    print(str(op) + '\n');
