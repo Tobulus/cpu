92610f6fbdc9d476dd31ae8383f5c39fa13acfb4
Tobsucht <tobiasbagg@googlemail.com>
2021-03-12 16:27:17 +0100

    adjust tests and bootloader after refactoring

--- assembler/test/test.asm
+++ assembler/test/test.asm
@@ -1,11 +1,11 @@
 add.u r3, r2, r1
 add.s r3, r2, r1
-addi.u r3, r2, 1
-addi.s r3, r2, -1
+add.u r3, r2, 1
+add.s r3, r2, -1
 sub.u r3, r2, r1
 sub.s r3, r2, r1
-subi.u r3, r2, 1
-subi.s r3, r2, -1
+sub.u r3, r2, 1
+sub.s r3, r2, -1
 or r3, r2, r1
 or r2, r1, 1
 and r3, r2, r1
@@ -24,9 +24,10 @@ write.b r2, r1, 1
 load.l r1, 1
 load.h r1, 1
 cmp r3, r2, r1
+cmp r5, r6, r2
 shiftl r2, r1, 1
 TEST:
 shiftr r2, r1, 1
-bi $TEST
+br $TEST
 br.eq r2, r1
-bro.gt r1, $TEST
+br.gt r1, $TEST
--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -9,21 +9,21 @@ load.h r4, 0
 UART_POLL:
 read.b r2, r1
 cmp r3, r2, r0
-bro.eq r3, $UART_POLL
+br.eq r3, $UART_POLL
 UART_RECV:
 read.b r2, r7
 cmp r5, r6, r2
 not r5, r5
-bro.eq r5, $CONTINUE
+br.eq r5, $CONTINUE
 load.l r5, 255
 load.h r5, 0
 cmp r3, r5, r2
-bro.eq r3, $START
+br.eq r3, $START
 CONTINUE:
-addi.u r6, r2, 0
+add.u r6, r2, 0
 write.b r4, r2
-addi.u r4, r4, 1
-bi $UART_POLL
+add.u r4, r4, 1
+br $UART_POLL
 START:
 cmp r0, r0, r0
 load.l r4, 100
